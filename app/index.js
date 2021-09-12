85dcafd1377f8612263165f0b1661caadb891736
chepakovich <chepakovich@gmail.com>
2021-09-12 11:35:22 -0700

    connected blockchain peers

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(express.json());
 
@@ -14,8 +17,8 @@ app.get('/blocks', (req, res) => {
 
 app.post('/mine', (req, res) => {
   const block = bc.addBlock(req.body.data);
-
   res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -2,6 +2,11 @@ const Websocket = require('ws');
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+const MESSAGE_TYPES = {
+  chain: 'CHAIN',
+  transaction: 'TRANSACTION',
+  clear_transactions: 'CLEAR_TRANSACTIONS',
+};
 
 class P2pServer {
   constructor(blockchain, transactionPool) {
@@ -26,4 +31,68 @@ class P2pServer {
       socket.on('open', () => this.connectSocket(socket));
     });
   }
+
+  connectSocket(socket) {
+    this.sockets.push(socket);
+    console.log('Socket connected');
+
+    this.messageHandler(socket);
+
+    this.sendChain(socket);
+  }
+
+  messageHandler(socket) {
+    socket.on('message', (message) => {
+      const data = JSON.parse(message);
+      switch (data.type) {
+        case MESSAGE_TYPES.chain:
+          this.blockchain.replaceChain(data.chain);
+          break;
+        case MESSAGE_TYPES.transaction:
+          this.transactionPool.updateOrAddTransaction(data.transaction);
+          break;
+        case MESSAGE_TYPES.clear_transactions:
+          this.transactionPool.clear();
+          break;
+      }
+    });
+  }
+
+  sendChain(socket) {
+    socket.send(
+      JSON.stringify({
+        type: MESSAGE_TYPES.chain,
+        chain: this.blockchain.chain,
+      })
+    );
+  }
+
+  sendTransaction(socket, transaction) {
+    socket.send(
+      JSON.stringify({
+        type: MESSAGE_TYPES.transaction,
+        transaction,
+      })
+    );
+  }
+
+  syncChains() {
+    this.sockets.forEach((socket) => this.sendChain(socket));
+  }
+
+  broadcastTransaction(transaction) {
+    this.sockets.forEach((socket) => this.sendTransaction(socket, transaction));
+  }
+
+  broadcastClearTransactions() {
+    this.sockets.forEach((socket) =>
+      socket.send(
+        JSON.stringify({
+          type: MESSAGE_TYPES.clear_transactions,
+        })
+      )
+    );
+  }
 }
+
+module.exports = P2pServer;
