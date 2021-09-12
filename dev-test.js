541138a2885a52e3604282c3f1839dab806f6532
chepakovich <chepakovich@gmail.com>
2021-09-11 17:14:40 -0700

    test commit

--- dev-test.js
+++ dev-test.js
@@ -1,8 +1,8 @@
 const Block = require('./block');
 
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
+
 // const block = new Block ('foo', 'bar', 'zoo', 'baz');
 // console.log(block.toString());
 // console.log(Block.genesis().toString());
-
-const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
-console.log(fooBlock.toString());
