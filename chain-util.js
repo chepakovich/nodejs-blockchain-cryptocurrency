eab8a62523711f0b3525c4f790a349926ad8c63f
chepakovich <chepakovich@gmail.com>
2021-09-14 13:47:13 -0700

    create a transaction

--- chain-util.js
+++ chain-util.js
@@ -1,6 +1,7 @@
 const EC = require('elliptic').ec;
 const SHA256 = require('crypto-js/sha256');
-// const uuidV1 = require('uuid/v1');
+const { v1 } = require('uuid');
+const uuidV1 = v1;
 const ec = new EC('secp256k1');
 
 class ChainUtil {
@@ -8,9 +9,9 @@ class ChainUtil {
     return ec.genKeyPair();
   }
 
-  // static id() {
-  //   return uuidV1();
-  // }
+  static id() {
+    return uuidV1();
+  }
 
   static hash(data) {
     return SHA256(JSON.stringify(data)).toString();
